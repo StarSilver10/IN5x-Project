417512d6d6b0d28734bfbcacd4620b3909e964c9
Tiboiser <globule.8895.67@gmail.com>
2020-12-19 16:55:46 +0100

    Adding new images

--- OpenCVTest.cpp
+++ OpenCVTest.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
-#include "plot.h"
 #include <fstream>
 
 using namespace std;
 using namespace cv;
 
-
+void writeCSV(string, Mat);
 
 int main()
 {
-	Mat img = imread("C:/Users/nigro/source/repos/OpenCVTest/OpenCVTest/src/Mary.jpg");
+	// Mat img = imread("C:/Users/nigro/source/repos/OpenCVTest/OpenCVTest/src/Mary.jpg");
+	Mat img = imread("mary.jpg");
+	if (img.empty()) {
+		std::cout << "Error loading img";
+		return -1;
+	}
 	namedWindow("image", WINDOW_NORMAL);
 	imshow("image", img);
 
@@ -18,12 +22,12 @@ int main()
 	threshold(img, imgBin, 127, 255, THRESH_BINARY_INV); // Binarisation
 	namedWindow("binarised image", WINDOW_NORMAL);
 	imshow("binarised image", imgBin);
-	
+
 	Mat horizProj;
-	reduce(imgBin, horizProj, 1, REDUCE_SUM, CV_64F); //Projection verticale
-	cout << "horizProj = " << endl << " " << horizProj << endl << endl;//Affichage du résultat
-	writeCSV("projec.csv", horizProj); //Export en csv pour affichage
-	
+	reduce(imgBin, horizProj, 1, REDUCE_SUM, CV_64F); // Projection verticale
+	cout << "horizProj = " << endl << " " << horizProj << endl << endl; // Affichage du résultat
+	writeCSV("project.csv", horizProj); // Export en csv pour affichage
+
 	waitKey(0);
 	return 0;
 }
